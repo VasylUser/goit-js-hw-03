702216b4316858137845567c2d63a1f3f9207ead
Vasyl <rkbarbaris.vasyl@gmail.com>
2025-04-20 09:51:54 +0200

    reworked Homework

--- js/task-3.js
+++ js/task-3.js
@@ -1,6 +1,6 @@
 function filterArray(numbers, value) {
     const valueArray = [];
-    for (let i = 0; i <= numbers.length; i++) {
+    for (let i = 0; i < numbers.length; i++) {
         if (numbers[i] > value) {
             valueArray.push(numbers[i]);            
         }
